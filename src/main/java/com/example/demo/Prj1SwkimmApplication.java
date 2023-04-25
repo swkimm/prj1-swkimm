7b75fdff426c6c9219c5e860e4288fff34e4b806
swkimm <swkim7161@kakao.com>
2023-04-25 16:43:06 +0900

    first

---
+++ src/main/java/com/example/demo/Prj1SwkimmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj1SwkimmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prj1SwkimmApplication.class, args);
+	}
+
+}
